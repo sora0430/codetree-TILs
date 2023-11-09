21a7c9f1a5d46fd911bd7e34a7a614ab8f801dc5
SORA0430 <42962117+sora0430@users.noreply.github.com>
2023-11-09 14:36:40 +0900

    Title: 정수만 추출하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231109/정수만 추출하기/extract-only-integers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    char c[2][9];
+    scanf("%s %s", c[0], c[1]);
+    int i, j, k;
+    int sum = 0;
+    for(i=0;i<2;i++) {
+        for(j=0;j<8;j++){
+            if(!('0'<=c[i][j]&&c[i][j]<='9')) {
+                int temp = 0;
+                for(k=0;k<j;k++) {
+                    temp *= 10;
+                    temp += c[i][k]-'0';
+                }
+                sum += temp;
+                break;
+            }
+        }
+    }
+    
+    printf("%d", sum);
+    return 0;
+}
