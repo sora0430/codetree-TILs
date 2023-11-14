03941671a0d65b497bdcafb8d196b95053612512
SORA0430 <42962117+sora0430@users.noreply.github.com>
2023-11-14 17:36:05 +0900

    Title: 진법 변환 4, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231114/진법 변환 4/base-conversion-4.c
+++ 231114/진법 변환 4/base-conversion-4.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
-
+#include <string.h>
 int main() {
-    int n;
+    char n[35];
     int r = 0;
     int m = 1;
-    scanf("%d", &n);
-    while(n > 0) {
-        int temp = n%10;
-        r += temp*m;
+    scanf("%s", n);
+    int i;
+    for(i=strlen(n)-1;i>=0;i--) {
+        if(n[i] == '1') r += m;
         m*=2;
-        n/=10;
     }
+
     printf("%d", r);
     return 0;
 }
