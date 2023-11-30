28ee90babb36b4dd137b28e07ca92166be5a09ae
SORA0430 <42962117+sora0430@users.noreply.github.com>
2023-11-30 14:37:53 +0900

    Title: 연속부분수열일까, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231130/연속부분수열일까/contiguous-array-or-not.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int l1, l2;
+    scanf("%d %d", &l1, &l2);
+    int *n1 = (int *)malloc(sizeof(int)*l1);
+    int *n2 = (int *)malloc(sizeof(int)*l2);
+
+    int i;
+    for(i=0;i<l1;i++) {
+        scanf("%d", &n1[i]);
+    }
+    for(i=0;i<l2;i++) {
+        scanf("%d", &n2[i]);
+    }
+    
+    int valid = 0;
+    int j, k;
+    for(i=0;i<l1;i++) {
+        for(j=0;j<l2;j++) {
+            if(n1[i]==n2[j]) {
+                if(l1-i < l2-j) {
+                    valid = -1;
+                    break;
+                }
+                valid = 1;
+                for(k=0;k<l2-j;k++) {
+                    if(n1[i+k]!=n2[j+k]) {
+                        valid = -1;
+                        break;
+                    }
+                }
+            }
+            if(valid != 0) break;
+        }
+        if(valid != 0) break;
+    }
+    if(valid != 1) printf("No");
+    else printf("Yes");
+    return 0;
+}
