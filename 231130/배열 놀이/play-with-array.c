f21038779722252745b95c0b380eb6ae84930fde
SORA0430 <42962117+sora0430@users.noreply.github.com>
2023-11-30 13:56:14 +0900

    Title: 배열 놀이, Time: 5ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 231130/배열 놀이/play-with-array.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n, q;
+    int *arr;
+    scanf("%d %d", &n, &q);
+    arr = (int *)malloc(sizeof(int)*q);
+    int i;
+
+    for(i=0;i<n;i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int j;
+    for(i=0;i<n;i++) {
+        int qType;
+        int a, b;
+        scanf("%d", &qType);
+        switch(qType) {
+            case 1:
+                scanf("%d", &a);
+                printf("%d\n", arr[a-1]);
+                break;
+            case 2:
+                scanf("%d", &a);
+                for(j=0;j<n;j++) {
+                    if(arr[j]==a) printf("%d\n", j+1);
+                }
+                break;
+            case 3:
+                scanf("%d %d", &a, &b);
+                for(j=a-1;j<=b-1;j++) {
+                    printf("%d ", arr[j]);
+                }
+                printf("\n");
+                break;
+        }
+    }
+    return 0;
+}
